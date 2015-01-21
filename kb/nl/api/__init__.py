c7569416b9f0b234501cd0c6a3ba1a3e0dc36924
WillemJan Faber <WillemJan.Faber@kb.nl>
2015-01-21 17:18:32 +0100

    Added packages toplevels

--- kb/nl/api/__init__.py
+++ kb/nl/api/__init__.py
@@ -1,7 +1,7 @@
-__version__ = '0.1dev'
+__version__ = '0.1.1'
 __all__ = ['oai']
 __author__ = 'WillemJan Faber <willemjan.faber@kb.nl'
 
-from .oai import oai
+from kb.nl.api import oai
 
 oai = oai()
